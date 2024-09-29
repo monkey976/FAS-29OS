0f2f9da05de76fd41d9aa5a5a64521a98e5eb9df
wushaomin <wushaomin976@yeah.net>
2024-09-29 16:46:26 +0800

    主界面优化

--- src/views/HomeUser/Menu.vue
+++ src/views/HomeUser/Menu.vue
@@ -1,25 +1,83 @@
 <template>
   <div>
     <!-- logo和项目名称 -->
-    <div class="flex items-center relative pt-15px">
-      <img src="../../assets/img/logo.png" alt="" id="logoImg" />
-      <span class="text-20px font-bold">FAS-29OS</span>
+    <div class="flex common-layout justify-between items-center">
       <div>
-        <el-avatar src="/src/assets/img/userImg.png" />
+        <div class="flex items-center relative pt-15px">
+          <img src="../../assets/img/logo.png" alt="" id="logoImg" />
+          <span class="text-20px font-bold">FAS-29OS</span>
+        </div>
       </div>
-      <el-dropdown @command="handleCommand">
-        <span class="el-dropdown-link">
-          {{ userName }}
-          <i class="el-icon-arrow-down el-icon--right"></i>
-        </span>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item command="profile">个人资料</el-dropdown-item>
-            <el-dropdown-item command="logout">退出登录</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
+      <div>
+        <div class="flex items-center relative pt-15px">
+          <div>
+            <el-avatar src="/src/assets/img/userImg.png" />
+          </div>
+          <el-dropdown @command="handleCommand">
+            <span class="el-dropdown-link">
+              wushaomin
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <!-- 个人信息 -->
+                <el-dropdown-item @click="viewProfile(userId)">个人信息</el-dropdown-item>
+                <!-- 修改密码 -->
+                <el-dropdown-item @click="changePassword">修改密码</el-dropdown-item>
+                <!-- 退出登录 -->
+                <el-dropdown-item @click="logout()">退出登录</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+          <el-dialog v-model="dialogVisible" title="个人信息" width="25%" draggable>
+            <!-- 用户详细信息展示 -->
+            <div class="flex items-center p-4 bg-white rounded-md">
+              <!-- 左侧头像 -->
+              <div class="w-24 h-24 flex justify-center items-center">
+                <el-avatar src="/src/assets/img/userImg.png" size="large" alt="User Avatar" />
+              </div>
+
+              <!-- 右侧用户信息 -->
+              <div class="w-auot">
+                <div class="space-y-4 text-gray-700 p-6 rounded-lg">
+                  <el-col :span="24">
+                    <p class="text-lg">
+                      <strong>用户名:</strong>
+                      <span class="text-gray-900">{{ myInfo.userName }}</span>
+                    </p>
+                  </el-col>
+                  <el-col :span="24">
+                    <p class="text-lg">
+                      <strong>电话号码:</strong>
+                      <span class="text-gray-900">{{ myInfo.phoneNumber }}</span>
+                    </p>
+                  </el-col>
+                  <el-col :span="24">
+                    <p class="text-lg">
+                      <strong>邮箱:</strong>
+                      <span class="text-gray-900">{{ myInfo.email }}</span>
+                    </p>
+                  </el-col>
+                  <el-col :span="24">
+                    <p class="text-lg">
+                      <strong>所属医院:</strong>
+                      <span class="text-gray-900">上海市虹桥医院</span>
+                    </p>
+                  </el-col>
+                  <el-col :span="24">
+                    <p class="text-lg">
+                      <strong>所属科室:</strong>
+                      <span class="text-gray-900">外科</span>
+                    </p>
+                  </el-col>
+                </div>
+              </div>
+            </div>
+          </el-dialog>
+        </div>
+      </div>
     </div>
+
     <el-menu :default-active="defaultActive" class="el-menu-demo" mode="horizontal">
       <template v-for="item in menuList" :key="item.id">
         <el-sub-menu v-if="item.children.length > 0" :index="item.id.toString()">
@@ -56,8 +114,11 @@ const refThis = ref(null)
 const isCollapse = ref(false) //菜单打开展示全还是不打开展示图标
 const defaultActive = ref(sessionStorage.getItem('path') || '/portaluser')
 
+const dialogVisible = ref(false)
 // 用户名数据，可以从后端获取
-const userName = ref('张三')
+const userName = ref('wushaomin')
+const userId = 18
+const userToken = 'wushaomin'
 
 // 处理下拉菜单选项的逻辑
 const handleCommand = (command: string) => {
@@ -127,6 +188,49 @@ const menuList = reactive([
 const savePath = (path) => {
   sessionStorage.setItem('path', path)
   router.push(path)
+}
+
+//实体类（登录）
+const myInfo = reactive({
+  userName: 'admin',
+  phoneNumber: 'admin123',
+  email: ''
+})
+
+// 个人信息的处理逻辑
+const viewProfile = async () => {
+  dialogVisible.value = true
+  try {
+    const response = await axios.get(
+      '/api/account/info',
+      {
+        params: {
+          userId: userId
+        }
+      },
+      { withCredentials: true }
+    )
+
+    if (response.code == 200) {
+      myInfo.userName = response.data.userName
+      myInfo.phoneNumber = response.data.phoneNumber
+      myInfo.email = response.data.email
+    } else {
+      dialogVisible.value = false
+      ElNotification({
+        title: '查询个人信息错误',
+        message: response.message,
+        type: 'error'
+      })
+    }
+  } catch (error) {
+    dialogVisible.value = false
+    ElNotification({
+      title: '查询个人信息错误',
+      message: error,
+      type: 'error'
+    })
+  }
 }
 </script>
 
